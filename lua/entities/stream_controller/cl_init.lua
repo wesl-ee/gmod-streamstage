8d85d3c869861d533df5989fbaced620cbc31fef
Yumi <yumi@prettyboytellem.com>
2020-04-12 23:36:41 -0400

    Unify VGUI

--- lua/entities/stream_controller/cl_init.lua
+++ lua/entities/stream_controller/cl_init.lua
@@ -4,64 +4,6 @@ function ENT:Draw()
 	self:DrawModel()
 end
 
-function ENT:DrawVGUINoOutput(p)
-	p:SetSize(520, 100)
-	local label = vgui.Create("DLabel", p)
-	label:SetPos(10, 30)
-
-	label:SetText("There are no speakers!")
-	label:SizeToContents()
-end
-
-function ENT:DrawVGUINotPlaying(p)
-	p:SetSize(520, 100)
-	local label = vgui.Create("DLabel", p)
-	label:SetPos(10, 30)
-
-	label:SetText("Stream URL")
-	label:SizeToContents()
-
-	local box = vgui.Create("DTextEntry", p)
-	box:SetPos(10, 50)
-	box:SetSize(500,20)
-	box.OnEnter = function(me)
-		net.Start("StartMusicStream")
-		net.WriteString(me:GetValue())
-		net.SendToServer()
-
-		self.URL = me:GetValue()
-		print("Self.URL ", self.URL)
-		me:GetParent():Close()
-	end
-
-	box:RequestFocus()
-end
-
-function ENT:DrawVGUINowPlaying(p)
-	p:SetSize(520, 100)
-	local label = vgui.Create("DLabel", p)
-	label:SetPos(10, 30)
-
-	label:SetText("Currently Playing!")
-	label:SizeToContents()
-
-	local box = vgui.Create("DTextEntry", p)
-	box:SetPos(10, 50)
-	box:SetSize(500,20)
-	print("Self.URL ", self.URL)
-	box:SetValue(self.URL)
-	box.OnEnter = function(me)
-		net.Start("StartMusicStream")
-		net.WriteString(me:GetValue())
-		net.SendToServer()
-
-		self.URL = me:GetValue()
-		me:GetParent():Close()
-	end
-
-	box:RequestFocus()
-end
-
 net.Receive("ShowControllerVGUI", function()
 	local controller = net.ReadEntity()
 	local netcodes = controller.NETCODE
@@ -75,11 +17,42 @@ net.Receive("ShowControllerVGUI", function()
 	frame.btnMaxim:SetVisible(false)
 
 	if nc == netcodes.VGUI_NO_OUTPUTS then
-		controller:DrawVGUINoOutput(frame)
-	elseif nc == netcodes.VGUI_NOT_PLAYING then
-		controller:DrawVGUINotPlaying(frame)
-	elseif nc == netcodes.VGUI_NOW_PLAYING then
-		controller:DrawVGUINowPlaying(frame)
+		frame:SetSize(520, 100)
+		local label = vgui.Create("DLabel", frame)
+		label:SetPos(10, 30)
+
+		label:SetText("There are no speakers!")
+		label:SizeToContents()
+	else
+		frame:SetSize(520, 100)
+
+		local box = vgui.Create("DTextEntry", frame)
+		box:SetPos(10, 50)
+		box:SetSize(500,20)
+
+		local label = vgui.Create("DLabel", frame)
+		label:SetPos(10, 30)
+
+		if nc == netcodes.VGUI_NOW_PLAYING then
+			box:SetValue(controller.URL)
+			label:SetText("Currently Playing")
+		else
+			label:SetText("Stream URL")
+		end
+
+
+		label:SizeToContents()
+
+		box.OnEnter = function(me)
+			net.Start("StartMusicStream")
+			net.WriteString(me:GetValue())
+			net.SendToServer()
+
+			controller.URL = me:GetValue()
+			me:GetParent():Close()
+		end
+
+		box:RequestFocus()
 	end
 
 	frame:Center()
